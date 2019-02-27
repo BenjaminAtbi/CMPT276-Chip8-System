abeff3344a2647e264f00133db277e633b34cfab
markom <markom@asb9838nu-c05.csil.sfu.ca>
2019-02-27 15:42:10 -0800

    Added parsing function

--- Chip8EmulatorProject/fileReader.js
+++ Chip8EmulatorProject/fileReader.js
@@ -1,3 +1,6 @@
+
+// reads the file from the input type=file with id inputFile and stores the contents
+//    in
 function fileReader(theEvent) {
   var theFile = theEvent.target.files[0]; // get the file
   var reader = new FileReader(); // create FileReader()
@@ -6,8 +9,15 @@ function fileReader(theEvent) {
     var theText = reader.result; // get the file data from the reader
     document.getElementById("FileInputDisplay").innerText = theText; // sets text of "display" element to the result of file
     //console.log(theText);
+    console.log(parseIt(theText));
   }
   reader.readAsText(theFile); // option for output
+}
+
+// takes a string and parses it into an array with groups of 4 using a regular expression
+function parseIt(content) {
+  var regex = /([A-Z]|[0-9]){1,4}/gi;
+  return content.match(regex);
 }
 
 // event listener for whenever the file button changes
