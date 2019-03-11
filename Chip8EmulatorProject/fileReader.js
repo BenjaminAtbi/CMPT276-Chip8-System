6ad214fd314a4946bbeea0233f6792fdfe6da64c
markom <markom@asb9838nu-d13.csil.sfu.ca>
2019-03-11 16:09:20 -0700

    update fileReader now parses to int

--- Chip8EmulatorProject/fileReader.js
+++ Chip8EmulatorProject/fileReader.js
@@ -1,6 +1,6 @@
 
 // reads the file from the input type=file with id inputFile and stores the contents
-//    in
+//    in an array of ints
 function fileReader(theEvent) {
   var theFile = theEvent.target.files[0]; // get the file
   var reader = new FileReader(); // create FileReader()
@@ -11,13 +11,22 @@ function fileReader(theEvent) {
     //console.log(theText);
     console.log(parseIt(theText));
   }
-  reader.readAsText(theFile); // option for output
+  reader.readAsText(theFile); // option for output type and format (URL, string, etc.)
 }
 
-// takes a string and parses it into an array with groups of 4 using a regular expression
+// takes a string and parses it into an array of integers (base 16)
+//    in groups of 4 using a regular expression
 function parseIt(content) {
-  var regex = /([A-Z]|[0-9]){1,4}/gi;
-  return content.match(regex);
+  var regex = /([A-Z]|[0-9]){1,4}/gi;   // letters A to Z or numbers 0 to 9 in groups of 4 case insensitive
+  var _4array = content.match(regex);   // split string into groups of 4
+  var opcodes = new Array;
+
+  for (i = 0; i < _4array.length; i++) {
+    opcodes.push(parseInt(_4array[i], 16));
+  }
+  return opcodes;
+
+  //return content.match(regex);
 }
 
 // event listener for whenever the file button changes
