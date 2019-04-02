779e05ef2fc53223d9a035b1ef44532884e7604d
Benjamin Atbi <benjaminatbi@gmail.com>
2019-04-01 20:02:39 -0700

    fixed selection of preloaded scripts

--- Chip8EmulatorProject/fileReader.js
+++ Chip8EmulatorProject/fileReader.js
@@ -1,21 +1,25 @@
-
-// reads the file from the input type=file with id inputFile and stores the contents
-//    in an array of ints
-function fileReader(theEvent) {
-  var theFile = theEvent.target.files[0]; // get the file
+function loadFile(file){
   var reader = new FileReader(); // create FileReader()
-
   reader.onload = function(anotherEvent) {
-    var theText = reader.result; // get the file data from the reader
-    document.getElementById("FileInputDisplay").innerText = theText; // sets text of "display" element to the result of file
-    //console.log(theText);
-    console.log(parseIt(theText));
-    chip8.loadProgram(parseIt(theText));    // loads the file into memory
+    var text = reader.result; // get the file data from the reader
+    printProgram(text)
+    console.log(text)
+    chip8.loadProgram(parseFile(text));    // loads the file into memory
   }
-  reader.readAsText(theFile); // option for output type and format (URL, string, etc.)
+  reader.readAsText(file); // option for output type and format (URL, string, etc.)
 }
 
-function parseIt(content) {
+function selectGame(e){
+  chip8.loadProgram(parseFile(preloadedScripts[e.target.value]))
+  console.log(preloadedScripts[e.target.value])
+}
+
+//to be improved
+function printProgram(text){
+  document.getElementById("FileInputDisplay").innerText = text; // sets text of "display" element to the result of file
+}
+
+function parseFile(content) {
   var regex = /([A-Z]|[0-9]){1,2}/gi;
   var _2array = content.match(regex);
   var opcodes = new Array;
@@ -26,23 +30,15 @@ function parseIt(content) {
   return opcodes;
 }
 
-//  ***DECPRECATED FUNCTION***
-// takes a string and parses it into an array of integers (base 16)
-//    in groups of 4 using a regular expression
-/*
-function parseIt(content) {
-  var regex = /([A-Z]|[0-9]){1,4}/gi;   // letters A to Z or numbers 0 to 9 in groups of 4 case insensitive
-  var _4array = content.match(regex);   // split string into groups of 4
-  var opcodes = new Array;
-
-  for (i = 0; i < _4array.length; i++) {
-    opcodes.push(parseInt(_4array[i], 16));
-  }
-  return opcodes;
-
-  //return content.match(regex);
+// reads the file from the input type=file with id inputFile and stores the contents
+//    in an array of ints
+function fileInputReader(theEvent) {
+  var theFile = theEvent.target.files[0]; // get the file
+  loadFile(theFile)
 }
-*/
+
 
 // event listener for whenever the file button changes
-document.getElementById("inputFile").addEventListener("change", fileReader, false);
+document.getElementById("inputFile").addEventListener("change", fileInputReader, false);
+document.getElementById("game_option").addEventListener("change",selectGame,false)
+
